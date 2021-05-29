19c218b76a5ed297e48ead3bdc64100dfe97f747
Tomek <tomasz.dworniczak@hotmail.com>
2021-05-29 22:35:32 +0200

    #1 Create task module DTO adn entity

---
+++ src/task/dto/task.dto.ts
@@ -0,0 +1,18 @@
+import { IsDate, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class TaskDto {
+  @IsNumber()
+  @IsOptional()
+  taskId: number;
+
+  @IsString()
+  taskName: string;
+
+  @IsDate()
+  @IsOptional()
+  startedTrackingDate: Date;
+
+  @IsDate()
+  @IsOptional()
+  stoppedTrackingDate: Date;
+}
