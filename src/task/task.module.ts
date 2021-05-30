278f820a445925a5ef0226088c76cec85435f288
Tomek <tomasz.dworniczak@hotmail.com>
2021-05-31 00:59:13 +0200

    #3 Add new providers for module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,14 +1,26 @@
 import { Module } from "@nestjs/common";
-import { TaskService } from "./services/task.service";
-import { TaskController } from "./controllers/task.controller";
+import { TaskService } from "./application/task.service";
+import { TaskController } from "./presentation/task.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Task } from "./entities/task.entity";
+import { TaskEntity } from "./infrastructure/task.entity";
+import { TaskTypeormRepository } from "./infrastructure/task.typeorm-repository";
+import { SystemTimeProvider } from "./application/systemTimeProvider";
+import { UuidEntityIdGenerator } from "./application/uuidEntityIdGenerator";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Task])
+    TypeOrmModule.forFeature([TaskEntity])
   ],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [TaskService, {
+    provide: "TaskRepository",
+    useClass: TaskTypeormRepository
+  }, { provide: "CurrentTimeProvider", useClass: SystemTimeProvider }, {
+    provide: "EntityIdGenerator",
+    useClass: UuidEntityIdGenerator
+  }]
 })
-export class TaskModule {}
+
+
+export class TaskModule {
+}
