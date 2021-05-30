5f7731e355ada0a75db5585d3fef5f500f248f3f
Tomek <50457289+tomaszdworniczak@users.noreply.github.com>
2021-05-30 12:13:40 +0200

    Task module (#5)

---
+++ src/task/task.controller.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TaskController } from './task.controller';
+import { TaskService } from './task.service';
+import { TaskDto } from "./dto/task.dto";
+
+describe('TaskController', () => {
+  let taskController: TaskController;
+  let taskService: TaskService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TaskController],
+      providers: [TaskService],
+    }).compile();
+
+    taskController = module.get<TaskController>(TaskController);
+    taskService = module.get<TaskService>(TaskService)
+  });
+
+  describe('findAll', () => {
+    it('should return an array of all tasks', async () => {
+      const result: TaskDto[] = [ { taskId: 123123, taskName: 'testTask', startedTrackingDate: new Date(), stoppedTrackingDate: new Date() },
+        { taskId: 456456, taskName: 'testTask2', startedTrackingDate: new Date(), stoppedTrackingDate: undefined } ];
+
+      jest.spyOn(taskService, 'findAll').mockImplementation(() => result);
+
+      expect(await taskController.findAll()).toBe(result);
+    });
+  });
+});
---
+++ src/task/task.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, HttpCode, Patch, Post } from "@nestjs/common";
+import { TaskService } from "./task.service";
+import { TaskDto } from "./dto/task.dto";
+
+@Controller('tracked-task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() taskDto: TaskDto) {
+    return this.taskService.create(taskDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get('current')
+  findCurrentTrackedTask() {
+    return this.taskService.findCurrentTrackedTask();
+  }
+
+  @Patch('current/stop')
+  @HttpCode(204)
+  stopTrackingCurrentTask() {
+    this.taskService.stopCurrentlyTrackedTask(new Date());
+  }
+}
