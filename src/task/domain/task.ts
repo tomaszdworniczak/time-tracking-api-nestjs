48a5e540a1aee130b6b8d6da7faac28c6ea77a83
Tomek <tomasz.dworniczak@hotmail.com>
2021-05-31 02:10:12 +0200

    #5 Add new static method

--- src/task/domain/task.ts
+++ src/task/domain/task.ts
@@ -1,8 +1,12 @@
 export class Task {
   private constructor(private readonly id: string, private readonly name: string, private readonly trackingStartedAt: Date, private readonly trackingStoppedAt: Date | undefined ){}
 
-  static startTracking(id: string, name: string,trackingStartedAt: Date): Task {
+  static startTracking(id: string, name: string, trackingStartedAt: Date): Task {
     return new Task(id, name, trackingStartedAt, undefined);
+  }
+
+  static fromEntity(id:string, name: string, trackingStartedAt: Date, trackingStoppedAt?: Date | undefined): Task {
+    return new Task(id, name, trackingStartedAt, trackingStoppedAt);
   }
 
   stopTracking(stoppedAt: Date): Task {
